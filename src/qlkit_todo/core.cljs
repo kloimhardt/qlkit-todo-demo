c694366d7a2ec83d9b665738840e72dcf8095c5a
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2021-05-07 19:52:16 +0200

    text animation

--- src/qlkit_todo/core.cljs
+++ src/qlkit_todo/core.cljs
@@ -6,7 +6,8 @@
             [qlkit-todo.parsers :refer [read mutate remote sync]]
             [qlkit-material-ui.core :refer [enable-material-ui!]]
             [cljs-http.client :as http :refer [post]]
-            [cljs.reader :refer [read-string]]))
+            [cljs.reader :refer [read-string]]
+            [goog.string :as gstring]))
 
 (enable-console-print!)
 (enable-material-ui!)
@@ -23,10 +24,26 @@
              [:icon/cancel {:on-click (fn []
                                         (transact! [:todo/delete!]))}]]]]))
 
+(def animation-delay 60)
+(def text
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate incidunt praesentium, rerum voluptatem in reiciendis officia harum repudiandae tempore suscipit ex ea, adipisci ab porro.")
+
+(defn make-text [css-class]
+  (let [f (fn [i x]
+            [:span {:class css-class
+                    :style {:animationDelay (str (* i animation-delay) "ms")}}
+             (if (= x " ") (gstring/unescapeEntities "&nbsp;") x)])]
+    (into [:p] (map-indexed f text))))
+
+(defcomponent New
+  (render []
+          [:div (make-text "char-out")]))
+
 (defcomponent TodoList
   (query [[:qlkit-todo/todos (ql/get-query TodoItem)]])
   (render [{:keys [:qlkit-todo/todos] :as atts} {:keys [new-todo] :as state}]
           [:div {:max-width 300}
+           [New]
            [:input {:id          :new-todo
                     :value       (or new-todo "")
                     :placeholder "What needs to be done?"
@@ -39,7 +56,7 @@
                                    (update-state! assoc :new-todo (.-value (.-target e))))}]
            (when (seq todos)
              [:card [:list (for [todo todos]
-                        [TodoItem todo])]])]))
+                             [TodoItem todo])]])]))
 
 (defn remote-handler [query callback]
   (go (let [{:keys [status body] :as result} (<! (post "endpoint" {:edn-params query}))]
